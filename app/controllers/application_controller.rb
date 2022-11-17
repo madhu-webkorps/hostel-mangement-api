ee45f895427d9bf2077fde4f3bf8bb939b50adbd
madhu-webkorps <madhu@webkorps.com>
2022-11-17 15:14:48 +0530

    initial

---
+++ app/controllers/application_controller.rb
@@ -0,0 +1,12 @@
+class ApplicationController < ActionController::Base
+  protect_from_forgery with: :exception
+
+  def after_sign_in_path_for(resource)
+    # if current_user.role == 'admin'
+    #   # admin_index_path
+    # else
+      root_path
+    # end
+  end
+
+end
