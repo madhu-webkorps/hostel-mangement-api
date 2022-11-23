7355cb242019598a0e344ea02d0be7be66bb76eb
madhu-webkorps <madhu@webkorps.com>
2022-11-23 11:44:16 +0530

    rooms work done

--- db/migrate/20221117094353_create_rooms.rb
+++ db/migrate/20221117094353_create_rooms.rb
@@ -1,6 +1,10 @@
 class CreateRooms < ActiveRecord::Migration[5.1]
   def change
     create_table :rooms do |t|
+    t.integer :seater
+    t.decimal :fees 
+    t.integer :room_no
+    t.references :hostel
 
       t.timestamps
     end
